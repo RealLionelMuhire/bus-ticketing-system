8a26be71d53b69328b74c180ab0f5d74fcd2bfef
RealLionelMuhire <muhirelionel@gmail.com>
2025-03-05 15:45:42 +0200

    adding another microservice for ticketing

---
+++ bus-ticketing-live-tracking-service/src/main/java/com/cavgo/busticketing/livetracking/BusTicketingLiveTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cavgo.busticketing.livetracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketingLiveTrackingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketingLiveTrackingServiceApplication.class, args);
+	}
+
+}
